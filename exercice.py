9c10c066d20c0e03cd167331d65bccba934b321c
AymenCheb <chebbi.aymen@hotmail.com>
2020-09-15 22:54:45 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -33,6 +33,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     splited_sentence = sentence.split()
+    for i
     pass
 
 
