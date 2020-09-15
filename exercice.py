214cd1c3f398c5be52121b9b3498a0e85a7a7216
AymenCheb <chebbi.aymen@hotmail.com>
2020-09-15 22:57:49 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    string = string[:2] + string[4:]
+    string = string[:2] + string[3:]
     return string
     pass
 
